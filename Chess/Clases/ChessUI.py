3a4209d5cd28e6e4eb255fa03ca55d3108bd6bee
Agustin Trachta <agustrachta@gmail.com>
2024-04-25 19:51:27 -0400

    objetos

--- Chess/Clases/ChessUI.py
+++ Chess/Clases/ChessUI.py
@@ -2,6 +2,8 @@ import pygame
 import os
 import sys
 
+from Clases.Piezas.Peon import Peon
+
 class ChessUI:
     def __init__(self, window_width=600, window_height=600):
         # Inicialización de Pygame
@@ -28,26 +30,13 @@ class ChessUI:
         #Tamaño de los peones
         self.piezas_size = (50, 50)  # Anchura y altura deseada para los peones
         
-        # Cargar imágenes de los peones blancos y negros
-        self.white_pawn = pygame.image.load(os.path.join(self.images_dir, "white pawn.png"))
-        self.black_pawn = pygame.image.load(os.path.join(self.images_dir, "black pawn.png"))
-    
+        #Peones
+        self.peon = Peon()
         # Cargar imágenes de los peones blancos y negros y ajustar su tamaño
-        self.white_pawn = pygame.transform.scale(pygame.image.load(os.path.join(self.images_dir, "white pawn.png")), self.piezas_size)
-        self.black_pawn = pygame.transform.scale(pygame.image.load(os.path.join(self.images_dir, "black pawn.png")), self.piezas_size)
+        self.white_pawn = pygame.transform.scale(self.peon.image_white, self.piezas_size)
+        self.black_pawn = pygame.transform.scale(self.peon.image_black, self.piezas_size)
 
-        #Alfiles
-        self.white_rook = pygame.image.load(os.path.join(self.images_dir, "white rook.png"))
-        self.black_rook = pygame.image.load(os.path.join(self.images_dir, "black rook.png"))
-        self.white_rook = pygame.transform.scale(pygame.image.load(os.path.join(self.images_dir, "white rook.png")), self.piezas_size)
-        self.black_rook = pygame.transform.scale(pygame.image.load(os.path.join(self.images_dir, "black rook.png")), self.piezas_size)
-        
-        #Caballos
-        self.white_knight = pygame.image.load(os.path.join(self.images_dir, "white knight.png"))
-        self.black_knight = pygame.image.load(os.path.join(self.images_dir, "black knight.png"))
-        self.white_knight = pygame.transform.scale(pygame.image.load(os.path.join(self.images_dir, "white knight.png")), self.piezas_size)
-        self.black_knight = pygame.transform.scale(pygame.image.load(os.path.join(self.images_dir, "black knight.png")), self.piezas_size)
-         
+
     def draw_chessboard(self):
         for row in range(8):
             for col in range(8):
@@ -59,35 +48,16 @@ class ChessUI:
 
     def draw_pawns(self):
         for row in range(8):
-            # Dibujar peones blancos en la segunda fila
             if row == 1:
                 for col in range(8):
                     x = col * self.square_size + (self.square_size - self.piezas_size[0]) // 2
                     y = row * self.square_size + (self.square_size - self.piezas_size[0]) // 2 
                     self.window.blit(self.white_pawn, (x, y))
-            # Dibujar peones negros en la séptima fila
             elif row == 6:
                 for col in range(8):
                     x = col * self.square_size + (self.square_size - self.piezas_size[0]) // 2
                     y = row * self.square_size + (self.square_size - self.piezas_size[1]) // 2
                     self.window.blit(self.black_pawn, (x, y))
-                    
-    def draw_rooks(self):
-        # Dibujar torres blancas en las esquinas superiores
-        self.window.blit(self.white_rook, (self.square_size // 2 - self.piezas_size[0] // 2, self.square_size // 2 - self.piezas_size[1] // 2))
-        self.window.blit(self.white_rook, ((7 * self.square_size) + self.square_size // 2 - self.piezas_size[0] // 2, self.square_size // 2 - self.piezas_size[1] // 2))
-        # Dibujar torres negras en las esquinas inferiores
-        self.window.blit(self.black_rook, (self.square_size // 2 - self.piezas_size[0] // 2, (7 * self.square_size) + self.square_size // 2 - self.piezas_size[1] // 2))
-        self.window.blit(self.black_rook, ((7 * self.square_size) + self.square_size // 2 - self.piezas_size[0] // 2, (7 * self.square_size) + self.square_size // 2 - self.piezas_size[1] // 2))
-
-    def draw_knights(self):
-        # Dibujar caballos blancos en las esquinas superiores
-        self.window.blit(self.white_knight, (self.square_size + self.square_size // 2 - self.piezas_size[0] // 2, self.square_size // 2 - self.piezas_size[1] // 2))
-        self.window.blit(self.white_knight, ((6 * self.square_size) + self.square_size // 2 - self.piezas_size[0] // 2, self.square_size // 2 - self.piezas_size[1] // 2))
-        # Dibujar caballos negros en las esquinas inferiores
-        self.window.blit(self.black_knight, (self.square_size + self.square_size // 2 - self.piezas_size[0] // 2, (7 * self.square_size) + self.square_size // 2 - self.piezas_size[1] // 2))
-        self.window.blit(self.black_knight, ((6 * self.square_size) + self.square_size // 2 - self.piezas_size[0] // 2, (7 * self.square_size) + self.square_size // 2 - self.piezas_size[1] // 2))
-
 
     
     def run(self):
@@ -104,8 +74,8 @@ class ChessUI:
 
             # Dibujar peones en el tablero
             self.draw_pawns()
-            self.draw_rooks()
-            self.draw_knights()
+            #self.draw_rooks()
+            #self.draw_knights()
 
             # Actualizar la pantalla
             pygame.display.update()
--- Chess/Clases/Piezas/Peon.py
+++ Chess/Clases/Piezas/Peon.py
@@ -1,3 +1,9 @@
+import pygame
+import os
+
 class Peon:
-    def __init__(self, color):
-        self.color = color
+    def __init__(self):
+        self.images_dir = os.path.join("Clases", "Imagenes")
+        self.image_white = pygame.image.load(os.path.join(self.images_dir, "white pawn.png"))
+        self.image_black = pygame.image.load(os.path.join(self.images_dir, "black pawn.png"))
+
